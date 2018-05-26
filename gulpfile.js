aea8d5c6fe303fc32f854aac924d6dcc708cfaaf
Alisa1711 <alisa.sp@gmail.com>
2018-05-26 03:27:31 +0300

    Добавлен блок features для всех разрешений и блок author для мобильного

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,23 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var browserSync = require('browser-sync');
+var server = require("browser-sync");
 
 gulp.task("style", function() {
   gulp.src("sass/style.scss")
     .pipe(sass())
     .pipe(postcss([autoprefixer()]))
     .pipe(gulp.dest("css"))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(server.stream());
 });
 
-gulp.task("browser-sync", function() {
-    browserSync({
-        server: {
-            baseDir: ""
-        },
-        notify: false
+gulp.task("server", function() {
+    server.init({
+      server: "",
+      notify: false,
+      open: true,
+      cors: true,
+      ui: false
     });
 });
 
